732d6d9c2776de7dcb1ea69935e508fe27706a67
Jaroslav Tavgen <jaroslav.tavgen@gmail.com>
2024-10-28 14:18:09 +0200

    Update words.js

--- words.js
+++ words.js
@@ -5,28 +5,30 @@ function splitIntoSentences(text) {
     return sentences ? sentences.map(sentence => sentence.trim()) : [];
 }
 
-let chasti_rechi = Array.from({length:10}, _=>[]);
+let chasti_rechi = Array.from({length:11}, _=>[]);
 
 let adjectives = 0;
 let adverbs = 1;
 let chastitsy = 2;
 let deeprichastiya = 3;
 let nimetives = 4;
-let prichastyya = 5;
-let prepositions = 6; 
-let unions = 7;
-let verbs = 8;
-let other = 9;
+let numerative = 5;
+let prichastyya = 6;
+let prepositions = 7; 
+let unions = 8;
+let verbs = 9;
+let other = 10;
 
-chasti_rechi[adjectives] = 'банковская банковские банковский банковских банковское больше годовом коммерческая коммерческие коммерческий коммерческое коммерческом корпоративный корпоративная корпоративное корпоративные лучшие намерен намерена огромный офисный офисная офисное первый прошлый эстонская эстонский эстонское эстонскому юридические юридическая юридические юридическое'.split(` `);
-chasti_rechi[adverbs] = 'всего еще когда несколько никак почти'.split(` `);
+
+chasti_rechi[adjectives] = 'банковская банковские банковский банковских банковское больше годовом коммерческая коммерческие коммерческий коммерческое коммерческом корпоративный корпоративная корпоративное корпоративные лучшие намерен намерена огромный офисный офисная офисное первый прошлый угольная  угольные угольный угольной эстонская эстонский эстонское эстонском эстонскому юридические юридическая юридические юридическое'.split(` `);
+chasti_rechi[adverbs] = 'все всего еще когда несколько никак ничего почти'.split(` `);
 chasti_rechi[chastitsy] = 'ли лишь не'.split(` `);
 chasti_rechi[deeprichastiya] = 'включая отмечая'.split(` `);
-chasti_rechi[nimetives] = 'другая другие другой им их какая-то какие-то какой-то мы нее он она они оно своем свою что этой этом этот'.split(` `);
+chasti_rechi[nimetives] = 'другая другие другой им их какая-то какие-то какой-то которая которого которое которые который которую мы нее он она они оно своем свою что этой этом этот'.split(` `);
 chasti_rechi[prichastyya] = 'действующая действующее действующей действующие действующий зарегистрирован зарегистрирована зарегистрированы'.split(` `);
 chasti_rechi[prepositions] = 'в до за к на от после с у'.split(` `);
 chasti_rechi[unions] = 'а и'.split(` `);
-chasti_rechi[verbs] = 'был была были было достигала есть закончила объясняет ответившая ответившее ответившего ответивший ответившее отвечал отвечала отвечали отвечало отвечать отвечаю отказалась отказались отказалось отказался отказаться поинтересовались поинтересовалась поинтересовался поинтересоваться предоставлять предоставляет предоставлял предоставляла предоставляли предоставляло продолжать снизился составил уполномочил уполномочила уполномочили'.split(` `);
+chasti_rechi[verbs] = 'был была были было достигала есть закончила заявил заявила заявили заявить объясняет ответившая ответившее ответившего ответивший ответившее отвечал отвечала отвечали отвечало отвечать отвечаю отказалась отказались отказалось отказался отказаться поинтересовались поинтересовалась поинтересовался поинтересоваться попросил попросить попрошу предоставлять предоставляет предоставлял предоставляла предоставляли предоставляло продолжать прокомментировал прокомментировать снизился составил уполномочил уполномочила уполномочили'.split(` `);
 
 document.addEventListener(`click`, a1)
 function a1(event)
@@ -35,7 +37,7 @@ function a1(event)
   let sentences = splitIntoSentences(event.target.innerText);
   for(let sentence=0;sentence<sentences.length;sentence++)
   {
-    let words = sentences[sentence].match(/[а-я\-]+/gi).map(e=>e.toLowerCase());
+    let words = sentences[sentence].match(/[а-я0-9\-]+/gi).map(e=>e.toLowerCase());
     let arr = Array.from({length:words.length}, _=>0);
     for(let word=0;word<words.length;word++)
     {
@@ -49,7 +51,16 @@ function a1(event)
           break
         }
       }
-      if(!found && !nouns.includes(words[word])) nouns.push(words[word]);
+      if(!found)
+      {
+        if(words[word].match(/\d+/gi) && (words[word] == words[word].match(/\d+/gi)))
+        {
+          arr[word] = numerative;
+        }
+        else{
+          if(!nouns.includes(words[word])) nouns.push(words[word]);
+        }
+      }
     }
   }
   console.log(nouns);
