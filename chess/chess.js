e2e8732a90ef52fe71094aa748c14415a6e5f732
jaroslavtavgen <jaroslav.tavgen@gmail.com>
2025-08-06 23:04:04 +0300

    JavaScript Chess Engine Lesson 4

---
+++ chess/chess.js
@@ -0,0 +1,23 @@
+let files, ranks;
+
+function init(){
+	console.log(`init() called`);
+	initiateFilesAndRanks();
+}
+
+function initiateFilesAndRanks(){
+	files = Array.from({length: 120}, _=>100);
+	ranks = Array.from({length: 120}, _=>100);	
+	for ( let rank = 0; rank < 8; rank++ ) {
+		for ( let file = 0; file < 8; file++ ) {
+			files [ rank * 10 + file + 21 ] = file;
+			ranks [ rank * 10 + file + 21 ] = rank;
+		}
+	}
+	console.log(`files[0]:${files[0]} ranks[0]:${ranks[0]}`)	
+	console.log(`files[21]:${files[21]} ranks[21]:${ranks[21]}`)	
+	console.log(`files[95]:${files[95]} ranks[95]:${ranks[95]}`)
+}
+
+init();
+console.log(`Main Init called`);
