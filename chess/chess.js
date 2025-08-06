edfdcd1d71216e8d63ed0b75ab59d316c2e4a8eb
jaroslavtavgen <jaroslav.tavgen@gmail.com>
2025-08-06 23:27:06 +0300

    JavaScript Chess Engine Lesson 4

--- chess/chess.js
+++ chess/chess.js
@@ -2,8 +2,19 @@ board = Array.from({length: 120}, _=>100);
 castlePermissions = 0;
 fiftyMoves = 0;
 hisPly = 0;
+pieceColors = [2, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1];
+pieceIsABishopOrAQueen = [false, false, false, true, false, true, false, false, false, true, false, true, false];
+pieceIsAKing = [false, false, false, false, false, false, true, false, false, false, false, false, true];
+pieceIsAKnight = [false, false, true, false, false, false, false, false, true, false, false, false, false];
+pieceIsAPawn = [false, true, false, false, false, false, false, true, false, false, false, false, false];
+pieceIsARookOrAQueen = [false, false, false, false, true, true, false, false, false, false, true, true, false];
+pieceSlides = [false, false, false, true, true, true, false, false, false, true, true, true, false];
+pieceValues = [0, 100, 325, 325, 500, 900, 50000, 100, 325, 325, 500, 900, 50000];
 ply = 0;
 side = 0;
+thisIsABigPiece = [false, false, true, true, true, true, true, false, true, true, true, true, true];
+thisIsAMajorPeece = [false, false, false, false, true, true, true, false, false, false, true, true, true];
+thisIsAMinorPIece = [false, false, true, true, false, false, false, false, true, true, false, false, false];
 
 function init(){
 	console.log(`init() called`);
