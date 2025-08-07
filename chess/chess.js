5f61c3bc782463aa500f364144b25c98f8918b04
jaroslavtavgen <jaroslav.tavgen@gmail.com>
2025-08-07 10:52:09 +0300

    JavaScript Chess Engine Lesson 7

--- chess/chess.js
+++ chess/chess.js
@@ -1,5 +1,6 @@
 board = Array.from({length: 120}, _=>100);
 castlePermissions = 0;
+enPassant = 0;
 fiftyMoves = 0;
 hisPly = 0;
 material = [0,0];
@@ -14,10 +15,15 @@ pieceList = Array.from({length: 10 * 14}, _=>0);
 pieceSlides = [false, false, false, true, true, true, false, false, false, true, true, true, false];
 pieceValues = [0, 100, 325, 325, 500, 900, 50000, 100, 325, 325, 500, 900, 50000];
 ply = 0;
+positionKey = 0;
 side = 0;
 thisIsABigPiece = [false, false, true, true, true, true, true, false, true, true, true, true, true];
 thisIsAMajorPeece = [false, false, false, false, true, true, true, false, false, false, true, true, true];
 thisIsAMinorPIece = [false, false, true, true, false, false, false, false, true, true, false, false, false];
+
+function rand32(){
+	return ( Math.floor ( ( Math.random () * 255 ) + 1 ) << 23 ) | ( Math.floor ( ( Math.random () * 255 ) + 1 ) << 16 ) | ( Math.floor ( ( Math.random () * 255 ) + 1 ) << 8 ) | Math.floor ( ( Math.random () * 255 ) + 1 );
+}
 
 function init(){
 	console.log(`init() called`);
@@ -40,3 +46,19 @@ function initiateFilesAndRanks(){
 
 init();
 console.log(`Main Init called`);
+let piece1 = rand32();
+let piece2 = rand32();
+let piece3 = rand32();
+let piece4 = rand32();
+let key = 0;
+key ^= piece1;
+key ^= piece2;
+key ^= piece3;
+key ^= piece4;
+console.log(`key: ${key.toString(16)}`);
+key = 0;
+key ^= piece4;
+key ^= piece2;
+key ^= piece3;
+key ^= piece1;
+console.log(`key: ${key.toString(16)}`);
